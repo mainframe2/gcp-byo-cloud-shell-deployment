fcc46924cd578e1c156943b2dbf40f784069a0c4
Marko Grujić <marko.grujic@nutanix.com>
2019-07-22 10:16:03 +0200

    Enable compute engine API if not enabled previously; remove the correct folder during cleanup.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+gcloud services enable compute.googleapis.com --project $1
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.networkAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.securityAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.storageAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/dns.admin
-cd .. && rm -fR test-cloud-shell-deployment
+cd .. && rm -fR gcp-byo-cloud-shell-deployment
