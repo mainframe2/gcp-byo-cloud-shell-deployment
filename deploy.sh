821f040f736157f4a9cb7b947c7a2955dd2de5b8
Marko Grujić <marko.grujic@nutanix.com>
2019-08-01 09:59:05 +0200

    Enable Google Cloud DNS API in BYO account required for DJI.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 gcloud services enable compute.googleapis.com --project $1
+gcloud services enable dns.googleapis.com --project $1
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.networkAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.securityAdmin
