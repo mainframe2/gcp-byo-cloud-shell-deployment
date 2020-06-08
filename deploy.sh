9ebb47b7e892f4c6afcd71bbb3908c8e7eb8f9bb
Mark Joscelyne <mjoscelyne@fra.me>
2020-06-08 16:44:07 -0400

    Adding support for Frame Gov staging platform

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 gcloud services enable compute.googleapis.com --project $1
 gcloud services enable dns.googleapis.com --project $1
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.networkAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.securityAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.storageAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/dns.admin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-gov-stage@frame-gov-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-gov-stage@frame-gov-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-gov-stage@frame-gov-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.securityAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-gov-stage@frame-gov-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-gov-stage@frame-gov-customer-iaas-stage.iam.gserviceaccount.com --role=roles/dns.admin
 cd .. && rm -fR gcp-byo-cloud-shell-deployment
