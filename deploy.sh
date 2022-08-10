e88d3eee86bcaaed3774e0ca7bb2b255b29994bb
Igor Dzakula <igor.dzakula@nutanix.com>
2022-08-10 04:06:40 -0700

    Adding deploy.sh for frame-development-byo-2
    Adding deploy.sh for frame-development-byo-2 GCP account

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+gcloud services enable compute.googleapis.com --project $1
+gcloud services enable dns.googleapis.com --project $1
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-customer-iaas-dev@frame-byo-development-2.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-customer-iaas-dev@frame-byo-development-2.iam.gserviceaccount.com --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-customer-iaas-dev@frame-byo-development-2.iam.gserviceaccount.com --role=roles/compute.securityAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-customer-iaas-dev@frame-byo-development-2.iam.gserviceaccount.com --role=roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-customer-iaas-dev@frame-byo-development-2.iam.gserviceaccount.com --role=roles/dns.admin
+cd .. && rm -fR gcp-byo-cloud-shell-deployment
