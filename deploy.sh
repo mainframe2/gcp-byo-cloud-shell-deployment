4983a624da1897e65f8d0c1aa7cc04c87432f385
Ivan Blagojevic <iblagojevic@gmail.com>
2024-11-13 15:12:00 +0100

    add compute admin role

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@fra
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.storageAdmin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/dns.admin
 gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-prod@frame-customer-iaas-prod.iam.gserviceaccount.com --role=roles/compute.admin
 cd .. && rm -fR gcp-byo-cloud-shell-deployment
